5af9672ef76da405ea147b9702461bb3a9bb2e48
i.starobranskyy <i.starobranskyy@magneticone.com>
2018-03-23 12:26:44 +0200

    new landing

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         autoScrolling: true,
         verticalCentered: false,
         scrollOverflow: true,
+        slidesNavigation: true,
         scrollingSpeed: 400
     });
 
